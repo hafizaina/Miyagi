b224c584346a47daf7d5f4cdc4ae100e95dfd7e3
Hafiz Aina <hafiz8594@gmail.com>
2013-07-02 13:11:28 -0400

    New files

---
+++ WorkFromHere/MiyagiGamePiece/MiyagiGamePiece/MPoint3D.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HumanStorm.Miyagi.Framework
+{
+    public class MPoint3D
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        // Initializes a new MPoint3D with each component set to 0f
+        public MPoint3D()
+        {
+            X = 0f;
+            Y = 0f;
+            Z = 0f;
+        }
+
+        public MPoint3D(float xPos, float yPos, float zPos)
+        {
+            SetPosition(xPos, yPos, zPos);
+        }
+
+        // Sets the position for this object
+        public void SetPosition(float xPos, float yPos, float zPos)
+        {
+            this.X = xPos;
+            this.Y = yPos;
+            this.Z = zPos;
+        }
+
+        // Returns the position for this object
+        public MPoint3D GetPosition()
+        {
+            return new MPoint3D(this.X, this.Y, this.Z);
+        }
+
+        public override string ToString()
+        {
+            return "This object is positioned at: " + "X = " + this.X +
+                "Y = " + this.Y + "Z = " + this.Z;
+        }
+
+    }
+}
