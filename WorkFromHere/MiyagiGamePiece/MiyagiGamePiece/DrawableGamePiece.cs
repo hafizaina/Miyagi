2f7930c8ebee035928faa3ae82c9add5ae584eca
zsen49 <deldes44@gmail.com>
2013-07-15 21:42:23 -0400

    Progress made with DrawableGamePiece and logic of ideas for unit tests

--- WorkFromHere/MiyagiGamePiece/MiyagiGamePiece/DrawableGamePiece.cs
+++ WorkFromHere/MiyagiGamePiece/MiyagiGamePiece/DrawableGamePiece.cs
@@ -1,46 +1,65 @@
-// FILE: C:/Users/ginga/Desktop//DrawableGamePiece.cs
+
+//THINGS THAT CAUGHT MY ATTENTION:
+// 1. nameOfShape is being plugged in as a parameter, though it does not get assigned to any variable.
+// 2. the rectangle variables require ints for position. xpos and ypos are floats.
+// 3. I have no idea what to do with viewport.
+// 4. SharedSpriteBatch is not being assigned correctly.
+// 5. Questions about all methods.
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-
-
-
-
-// In this section you can add your own using directives
-    // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000C8F begin
-    // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000C8F end
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.GamerServices;
 
 /// <summary>
-    /// This is the XNA framework specific derived class.
-     /// </summary>
-     /// 
+/// This class Returns the absolute position of the top left corner of this DrawableGamePiece with 
+/// respect to the top left-corner of the Graphics Display device.
+/// </summary>
+/// 
 namespace HumanStorm.Miyagi.Framework
 {
 
-    private class DrawableGamePiece : BaseGamePiece, DrawableGameComponent
-    {
-        // Attributes
-
-        public Texture2D TextureForShape;
-
-        public Color ColorOfShapeAndMathExpression;
-
-        public Rectangle RectangleEnclosingThisImage;
-
-        public SpriteFont Font;
-
-        public Rectangle RectangleContainingThisObject;
-
-        public SpriteBatch SharedSpriteBatch;
-
-        public Rectangle ViewPort;
-
-        // Operations
+    public class DrawableGamePiece : BaseGamePiece
+    {   
+        
+        //Attributes
 
         /// <summary>
-        /// nameOfShape corresponds to the name of the image to load for the TextureForShape member.
-        /// 
-        /// mathExpression is the math expression (i.e., "-A") to display on the key-block.
+        /// The invisible rectangle enclosing this image.  Adjusting the size and position of this rectangle. 
+        ///Implementation Details:  The size and the position of this Rectangle stores the size and position of the image.  
+        ///So to change the size of the image, change the size of the rectangle.  And to change position of the image, 
+        ///change the position of the rectangle.
         /// </summary>
+        public Rectangle RectangleEnclosingThisObject;
+        
+        /// <summary>
+        /// The rectangle that encloses the image drawn on the screen representing a game piece.
+        ///Implementation Details:  This is the variable that should have it's position updated and size updated.  
+        ///this.RectangleEnclosingThisImage just 
+        ///returns a new rectangle whose values are taken from 
+        ///the width, height, and position of this variable.
+        /// </summary>
+        public Rectangle RectangleContainingThisObject;
+        
+        /// <summary>
+        /// The SpriteBatch to handle drawing on the display device.
+        ///Implementation Details:  The constructor should initialize this variable as 
+        ///this.SharedSpriteBatch = (SpriteBatch)this.Game.Services.GetService(typeof(SpriteBatch));
+        ///This comes from http://msdn.microsoft.com/en-us/library/microsoft.xna.framework.game.services.aspx.
+        /// </summary>
+        public SpriteBatch SharedSpriteBatch;
+        
+        /// <summary>
+        /// Returns the Rectangle that represents the ViewPort to display this object in.
+        ///Implementation Details:  Just declare this as a property and let the getter return.
+        ///(Rectangle)this.Game.Services.GetService(typeof(Rectangle));
+        /// </summary>
+        public Rectangle ViewPort;
+        
+        /// <summary>
+        ///The color of the shape, and the color of the math expression, to be drawn on the screen.
+        /// </summary>
+        public Color ColorOfShape;
+
         /// <param name="contentToDraw">
         /// </param>
         /// <param name="isContentToDrawAMathExpression">
@@ -59,15 +78,29 @@ namespace HumanStorm.Miyagi.Framework
         /// </param>
         /// <returns>
         /// </returns>
-        public DrawableGamePiece(string contentToDraw, bool isContentToDrawAMathExpression, int widthOfThisGamePiece, int heightOfGamePiece, float xPos, float yPos, float zPos)
-        {
-            // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000CA1 begin
-            // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000CA1 end
+        public DrawableGamePiece(Color colorOfGamePiece, SpriteBatch sharedSprite, string nameOfShape, int widthOfThisGamePiece, int 
+            heightOfGamePiece, float xPos, float yPos, float zPos)
+            : base
+                (widthOfThisGamePiece, heightOfGamePiece, xPos, yPos, zPos){
+            
+            this.ColorOfShape = colorOfGamePiece;
+            this.RectangleEnclosingThisObject = new Rectangle(0, 0, this.Width, this.Height);
+            this.RectangleContainingThisObject = new Rectangle(0, 0, this.Width, this.Height);
+            //------------------------ ATTTTTTTTTENNNNNNNNNNNNNNNNNNNNNTTTTTTTTTIIIIIIIIIOOOOOOOONNNNNNNNNN-------------------
+            // XPOS AND YPOS DATA TYPES ARE FLOATS. RECTANGLE CONSTRUCTOR ONLY ACCEPTS INTS.
+            //GREAT SUGGESTION WOULD BE TO REPLACE RECTANGLE DATA TYPE WITH RECT STRUCTURE DATA TYPE(RECT TAKES DOUBLES AS PARAMETERS).
+            //this.RectangleEnclosingThisObject = new Rect(xPos, yPos, this.Width, this.Height);
+            //THIS WOULD ALLOW MOVEMENT AS SPECIFIED IN IMPLEMENTATION.
+            //STILL DO NOT KNOW HOW TO USE THIS CLASS, THOUGH.
+            //-----------------------------------------------------------------------------------------------------------
 
+
+             this.SharedSpriteBatch = (SpriteBatch)this.Game.Services.GetService(typeof(SpriteBatch));
         }
 
         /// <summary>
-        /// Draws the key-block on the screen.  If IsTargeted is set to true, then the image drawn is scaled such that its width and height is now multplied by SCALE_FACTOR.  Otherwise, the image is drawn at its regular size specified by Width and Height.
+        /// Draws the key-block on the screen.  If IsTargeted is set to true, then the image drawn is scaled such that its width and height 
+        /// is now multplied by SCALE_FACTOR.  Otherwise, the image is drawn at its regular size specified by Width and Height.
         /// 
         /// Width - The width of the key-block
         /// Height - The height of the key-block.
@@ -82,17 +115,26 @@ namespace HumanStorm.Miyagi.Framework
         /// </returns>
         public void Draw(GameTime time)
         {
-            // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000CAA begin
-            // section 10-0-0-8-5845b79f:13f0ba8d3fc:-8000:0000000000000CAA end
-
+            if (IsTargeted == true)
+            {
+                //Draw the game piece with width and height affected by scale-factor. Does this mean make a new rectangle with specs
+                //multiplied by scale factor?
+            }
+            else
+            {
+                //leave it as is?
+            }
         }
 
         /// <summary>
-        /// Checks to determine if the input device is currently hovering over the image that this object displays.  This also updates the position of this object as well as 
+        /// Checks to determine if the input device is currently hovering over the image that this object displays.  This also updates 
+        /// the position of this object as well as 
         /// 
-        /// Implementation details:  If the input device is hovering over the image contained by this object, then set IsTargeted to true, otherwise false.  
+        /// Implementation details:  If the input device is hovering over the image contained by this object, then set 
+        /// IsTargeted to true, otherwise false.  
         /// 
-        /// Add logic to determine if "IsSelected" should be set to true or false depending on the interaction technique chosen.  For example, if using the mouse then check to see if the mouse button is pressed over this object.
+        /// Add logic to determine if "IsSelected" should be set to true or false depending on the interaction technique chosen.  
+        /// For example, if using the mouse then check to see if the mouse button is pressed over this object.
         /// 
         /// 
         /// This also updates the Position of this object as well as Size.  The Position gets updated only if IsSelected is set to true.
@@ -103,20 +145,24 @@ namespace HumanStorm.Miyagi.Framework
         /// </returns>
         public void Update(GameTime time)
         {
-            // section -64--88-56-1--5923b67a:13f818d274c:-8000:0000000000000B27 begin
-            // section -64--88-56-1--5923b67a:13f818d274c:-8000:0000000000000B27 end
-
+            
         }
 
         /// <summary>
-        /// Updates the position of the top left hand corner of this DrawableGamePiece as it is displayed on the screen With Respect To (WRT) the Viewport.  
+        /// Updates the position of the top left hand corner of this DrawableGamePiece as it is displayed on the screen
+        /// With Respect To (WRT) the Viewport.  
         /// 
-        /// The GamePiece is not allowed to leave the boundary of the ViewPort.  The Position is constrained so that no part of the GamePiece is outside the View Port.
+        /// The GamePiece is not allowed to leave the boundary of the ViewPort.  The Position is constrained so that no 
+        /// part of the GamePiece is outside the View Port.
         /// *********************
-        /// Implementation details:  This method should override the SetPosition provided by the parent class.  This is how you override a method http://msdn.microsoft.com/en-us/library/ebca9ah3.aspx.
-        /// In this method, just adjust the position of RectangleEnclosingThisImage with the parameters xPos & yPos.  Then update the "Position" from the parent class with the same value of xPos and yPos and zPos (which should be zero) passed to this method.
+        /// Implementation details:  This method should override the SetPosition provided by the parent class.  This is how you 
+        /// override a method http://msdn.microsoft.com/en-us/library/ebca9ah3.aspx.
+        /// In this method, just adjust the position of RectangleEnclosingThisImage with the parameters xPos & yPos.  Then update the 
+        /// "Position" from the parent class with the same value of xPos and yPos and zPos (which should be zero) passed to this method.
         /// 
-        /// Also this is With Respect to the ViewPort.  To accomplish this, whatever xPos, yPos is, just add the coordinates of the top left corner of the RectangleContainingThisObject to the xPos, and yPos, and then set the coordinates coordinates everywhere else.
+        /// Also this is With Respect to the ViewPort.  To accomplish this, whatever xPos, yPos is, just add the coordinates of 
+        /// the top left corner of the RectangleContainingThisObject to the xPos, and yPos, and then set the coordinates 
+        /// coordinates everywhere else.
         /// 
         /// </summary>
         /// <param name="xPos">
@@ -127,17 +173,17 @@ namespace HumanStorm.Miyagi.Framework
         /// </param>
         /// <returns>
         /// </returns>
-        public void SetPosition(float xPos, float yPos, float zPos)
+        public override void SetPosition(float xPos, float yPos, float zPos)
         {
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AD6 begin
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AD6 end
-
+            base.SetPosition(xPos, yPos, zPos);
         }
 
         /// <summary>
         /// Sets the width and height of this image, in pixels, as it is drawn on the screen.
         /// 
-        /// Implementation Details:  Set the parent Width and Height to the width and height parameters passed here.  Set the parent Width and Height.  Then reset the Width and Height of RectangleContainingThisObject to the with and height input into this method.
+        /// Implementation Details:  Set the parent Width and Height to the width and height parameters passed here.  
+        /// Set the parent Width and Height.  Then reset the Width and Height of RectangleContainingThisObject to the 
+        /// with and height input into this method.
         /// </summary>
         /// <param name="widthOfThisBlock">
         /// </param>
@@ -145,11 +191,11 @@ namespace HumanStorm.Miyagi.Framework
         /// </param>
         /// <returns>
         /// </returns>
-        public void SetSize(int widthOfThisBlock, int heightOfThisBlock)
+        public override void SetSize(int widthOfThisBlock, int heightOfThisBlock)
         {
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AE1 begin
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AE1 end
-
+            base.SetSize(widthOfThisBlock, heightOfThisBlock);
+            this.RectangleContainingThisObject.Width = widthOfThisBlock;
+            this.RectangleContainingThisObject.Height = heightOfThisBlock;
         }
 
         /// <summary>
@@ -163,9 +209,6 @@ namespace HumanStorm.Miyagi.Framework
         /// </returns>
         protected void LoadContent()
         {
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AF6 begin
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AF6 end
-
         }
 
         /// <summary>
@@ -175,20 +218,19 @@ namespace HumanStorm.Miyagi.Framework
         /// </returns>
         public Vector3 GetScreenCoordinatesOfMouse()
         {
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AF9 begin
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000AF9 end
-
+            Vector3 mouseState = new Vector3(Mouse.GetState().X, Mouse.GetState().Y,0);
+            return mouseState;
         }
 
         /// <summary>
-        /// Returns the absolute position of the top left corner of this DrawableGamePiece with respect to the top left-corner of the Graphics Display device.
+        /// Returns the absolute position of the top left corner of this DrawableGamePiece with respect to the top left-corner 
+        /// of the Graphics Display device.
         /// </summary>
         /// <returns>
         /// </returns>
         public Vector3 GetPositionWithRespectToViewPort()
         {
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000B0F begin
-            // section -64--88-56-1-72ba4d10:13f9ac2df4c:-8000:0000000000000B0F end
+            return new Vector3();
 
         }
 
@@ -199,9 +241,9 @@ namespace HumanStorm.Miyagi.Framework
         /// </summary>
         /// <returns>
         /// </returns>
-        public MPoint3D GetPosition()
+        public override MPoint3D GetPosition()
         {
-            return new MPoint3D(0, 0, 0);
-        }
+            return base.GetPosition();
+        }       
     } /* end class DrawableGamePiece */
 }
---
+++ WorkFromHere/MiyagiGamePiece/UnitTestsForDrawableGamePiece/DrawableGamePieceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTestsForDrawableGamePiece
+{
+    [TestFixture]
+    public class TestFixture1
+    {
+     //Test for getters.
+    //Test for constructor.
+        //Test for setters.
+    }
+}
